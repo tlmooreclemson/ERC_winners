0ac637418ceb60c6620fffc6ef29e02b7f969c02
Thomas Moore <tlmoore1928@gmail.com>
2020-10-21 13:03:22 +0200

    Code to pull 2020 ERC winners into a list

--- pyERCwinners.py
+++ pyERCwinners.py
@@ -4,4 +4,13 @@ Created on: Wed Sep 21 11:49 2020
 Created by: Thomas Moore
 """
 
+# Load modules
+from tabula import read_pdf
 
+# Import the data for ERC StG 2020 Winners
+file_path = "https://erc.europa.eu/sites/default/files/document/file/erc_2020_stg_results_all_domains.pdf"
+
+df = read_pdf(file_path, pages = "all")
+df = df[0::10]
+
+# Each page is on a multiple of 10, i.e. df[0], df[10], df[20]... etc.
