43c4a51f3cf7d3601a6a55cf086dfc96109d30b7
chiattomaglio <tlmoore1928@gmail.com>
2020-10-22 09:18:51 +0200

    New lines of code for pd data frame

--- pyERCwinners.py
+++ pyERCwinners.py
@@ -6,11 +6,17 @@ Created by: Thomas Moore
 
 # Load modules
 from tabula import read_pdf
+import pandas as pd
+
 
 # Import the data for ERC StG 2020 Winners
 file_path = "https://erc.europa.eu/sites/default/files/document/file/erc_2020_stg_results_all_domains.pdf"
 
 df = read_pdf(file_path, pages = "all")
-df = df[0::10]
+df = df[0::10] # extract only the relevant lists
+df = pd.concat(df) # concantate all the data frames into a single large one
 
-# Each page is on a multiple of 10, i.e. df[0], df[10], df[20]... etc.
+# Remove irrelevant rows
+df = df[~df['Last name'].isin(['Last name'])] # drop rows with 'last name'
+df = df[df['Last name'].notna()] # drop rows with NaN in 'last name' column
+
